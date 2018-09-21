12244a92bc359f4f16192457f4a3f8357eb77aea
Cara Salyers <carasalyers@gmail.com>
2018-09-21 14:46:36 -0400

    KP2 packages

---
+++ KP2.R
@@ -0,0 +1,27 @@
+#################
+#####  KP2  #####
+#################
+
+
+### Install Packages
+install.packages(c("psych", "foreign", "DBI", "readxl", "haven", "car", "effects", "corrplot", "BaylorEdPsych", "ggfortify", "cluster", "klaR", "rrcov", "gmodels", "QuantPsyc", "tidyverse"))
+
+### Load packages
+library(psych)
+library(foreign)
+library(DBI)
+library(readxl)
+library(haven)
+library(car)
+library(effects)
+library(corrplot)
+library(BaylorEdPsych)
+library(ggfortify)
+library(cluster)
+library(klaR)
+library(rrcov)
+library(gmodels)
+library(QuantPsyc)
+library(tidyverse)
+
+### Read Data
